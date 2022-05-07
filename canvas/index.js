b8659b3ce5e3ab268722d096be36ed1a1d0f43c4
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-05-07 20:38:06 -0300

    feat: add score to the game

--- canvas/index.js
+++ canvas/index.js
@@ -3,8 +3,8 @@ import GAME_STATE from "./gameState.js";
 document.addEventListener("DOMContentLoaded", main);
 
 /** @type {CanvasRenderingContext2D} */
-let borders = 0;
 let canvasContext = null;
+let borders = 0;
 let windowHeight = 0;
 let windowWidth = 0;
 
@@ -12,6 +12,7 @@ const baseWidth = 1280;
 const baseHeight = 720;
 const maxJumps = 3;
 let currentGameState = GAME_STATE.PLAY;
+let record = 0;
 
 /**
  * Hold the reference of previous values by a key
@@ -31,6 +32,7 @@ const player = {
     color: "#47fd",
     gravity: 1.6,
     velocity: 0,
+    score: 0,
     jumpConfig: {
         force: 23.6,
         quantity: 0
@@ -50,6 +52,17 @@ const player = {
             this.jumpConfig.quantity++;
         }
     },
+    reset: function() {
+        this.velocity = 0;
+        this.y = 0;
+
+        if (this.score > record) {
+            localStorage.setItem("record", this.score);
+            record = this.score;
+        }
+
+        this.score = 0;
+    },
 };
 
 const obstacle = {
@@ -64,6 +77,9 @@ const obstacle = {
     colors: ["#3beca8", "#fcc147", "#6c2d4e", "#def098", "#cd382f"],
     positions: [baseWidth / 2, baseWidth / 3, baseWidth / 1.2, baseWidth / 1.4, baseWidth / 1.6],
     insertTime: 0,
+    clean: function() {
+        this.allObstacles = [];
+    },
     create: function() {
         this.allObstacles.push({
             x: baseWidth, // getRandomElement(this.positions, 5, true, 'obstaclePositions'),
@@ -103,9 +119,8 @@ const obstacle = {
                 (player.y + player.height) >= (floor.y - obstacle.height)
             ) {
                 currentGameState = GAME_STATE.LOST;
-                this.allObstacles = [];
-                return;
-            }
+                this.clean();
+            } else if (obstacle.x === player.x) player.score++;
         });
     },
     draw: function() {
@@ -120,6 +135,14 @@ const obstacle = {
         });
     },
 };
+
+function main() {
+    setWindowDimensions();
+    configureCanvas();
+    configureScore();
+
+    run();
+}
 
 function run() {
     drawElements();
@@ -138,14 +161,11 @@ function click() {
 
     if (currentGameState === GAME_STATE.PLAYING) player.jump();
 
-    if (currentGameState === GAME_STATE.LOST) currentGameState = GAME_STATE.PLAY;
-}
-
-function main() {
-    setWindowDimensions();
-    configureCanvas();
-
-    run();
+    if (currentGameState === GAME_STATE.LOST) {
+        player.reset();
+        obstacle.clean();
+        currentGameState = GAME_STATE.PLAY;
+    }
 }
 
 function setWindowDimensions() {
@@ -169,6 +189,12 @@ function configureCanvas() {
     document.addEventListener("mousedown", click);
 }
 
+function configureScore() {
+    record = localStorage.getItem("record");
+
+    if (!record) record = 0;
+}
+
 function drawElements() {
     // Sky
     drawRectangle(0, 0, windowWidth, windowHeight, "#53d6ed");
@@ -184,13 +210,40 @@ function drawElements() {
         "#FF0000"
     );
 
+    // Current score
+    drawText(player.score || 'No score :(', 20, 100);
+
     if (currentGameState === GAME_STATE.PLAY) {
         drawRectangle(baseWidth / 2 - 50, baseHeight / 2 - 50, 100, 100, "green");
     }
 
     if (currentGameState === GAME_STATE.LOST) {
         drawRectangle(baseWidth / 2 - 50, baseHeight / 2 - 50, 100, 100, "red");
+        // Score
+        drawScore();
     }
+}
+
+function drawScore() {
+    canvasContext.save();
+
+    if (player.score > record)
+        drawText("New record!", (baseWidth / 2) - 125, (baseHeight / 2) - 100);
+    else if (record < 10)
+        drawText(`Record: ${record}`, (baseWidth / 2) - 120, (baseHeight / 2) - 100);
+    else if (record > 10 && record < 100)
+        drawText(`Record: ${record}`, (baseWidth / 2) - 120, (baseHeight / 2) - 100);
+    else
+        drawText(`Record: ${record}`, (baseWidth / 2) - 115, (baseHeight / 2) - 100);
+
+    if (player.score < 10)
+        drawText(player.score, baseWidth / 2 - 15, baseHeight / 2 + 20);
+    else if (player.score >= 10 && player.score < 100)
+        drawText(player.score, baseWidth / 2 - 30, baseHeight / 2 + 20);
+    else
+        drawText(player.score, baseWidth / 2 - 45, baseHeight / 2 + 20);
+
+    canvasContext.restore();
 }
 
 /**
@@ -209,6 +262,26 @@ function drawRectangle(x, y, width, height, color = "#000") {
 
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x, y, width, height);
+}
+
+/**
+ * @param {String} text 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {String} color 
+ * @param {String} fontSize 
+ * @param {String} fontFamily 
+ * @returns 
+ */
+function drawText(text, x, y, color = "#fff", fontSize = "50px", fontFamily = "Arial") {
+    if (!canvasContext) {
+        console.error('Context not defined');
+        return;
+    }
+
+    canvasContext.fillStyle = color;
+    canvasContext.font = `${fontSize} ${fontFamily}`;
+    canvasContext.fillText(text, x, y);
 }
 
 /**
